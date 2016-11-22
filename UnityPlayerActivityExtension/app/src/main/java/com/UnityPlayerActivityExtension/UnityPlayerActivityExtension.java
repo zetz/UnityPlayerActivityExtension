4ff7000d5b234b2246166cdc3ea769df6b766338
정광일 <laitdei@gmail.com>
2016-11-22 22:58:46 +0900

    UnitySendMessage Test

--- UnityPlayerActivityExtension/app/src/main/java/com/UnityPlayerActivityExtension/UnityPlayerActivityExtension.java
+++ UnityPlayerActivityExtension/app/src/main/java/com/UnityPlayerActivityExtension/UnityPlayerActivityExtension.java
@@ -3,6 +3,7 @@ package com.UnityPlayerActivityExtension;
 
 import com.unity3d.player.*;
 import android.os.Bundle;
+import android.content.Intent;
 
 public class UnityPlayerActivityExtension extends UnityPlayerActivity
 {
@@ -10,7 +11,14 @@ public class UnityPlayerActivityExtension extends UnityPlayerActivity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+		UnityPlayer.UnitySendMessage("EventSystem", "CallFromNative", "UnityPlayerActivityExtension Created.");
     }
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		UnityPlayer.UnitySendMessage("EventSystem", "CallActivityResult", requestCode + ", " + resultCode);
+	}
 
 	public String Hello(String msg)
 	{
