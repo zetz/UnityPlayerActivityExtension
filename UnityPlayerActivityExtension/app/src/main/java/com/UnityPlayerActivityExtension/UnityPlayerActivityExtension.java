00ec0e466153e84302d93e210efa8778b9d0fee1
정광일 <laitdei@gmail.com>
2016-11-22 21:52:50 +0900

    First Build

--- UnityPlayerActivityExtension/app/src/main/java/com/UnityPlayerActivityExtension/UnityPlayerActivityExtension.java
+++ UnityPlayerActivityExtension/app/src/main/java/com/UnityPlayerActivityExtension/UnityPlayerActivityExtension.java
@@ -7,13 +7,11 @@ import android.os.Bundle;
 
 public class UnityPlayerActivityExtension extends Activity
 {
-    /** 작업이 처음 만들어질 때 호출됩니다. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
 
-        /* TextView를 만들고 텍스트를 "Hello world"로 설정합니다.*/
         TextView  tv = new TextView(this);
         tv.setText("Hello World!");
         setContentView(tv);
